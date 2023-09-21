e7781b095ec1dd7f264067aceb413173ac0117dd
exhq <infidelLOLer@gmail.com>
2023-09-21 18:14:04 +0330

    hi

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+import Person from "./lib/person.svelte";
+
+document.addEventListener("keydown", (e)=> {
+console.log(e.key)
+})
+
+</script>
+
+<main>
+<center>
+  <Person discordid="330299657458679821"></Person>
+</center>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/person.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let discordid: string;
+    let name = "maggie";
+    const altnames = ["maggie 2", "maggie 3", "maggie 4"];
+
+    function getRandomChild(array) {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+    }
+</script>
+
+<img class="pfp" src="https://adu.shiggy.fun/v1/{discordid}/avatar" alt="" />
+<br />
+<h1 on:click={()=> {name = getRandomChild(altnames)}}>{name}</h1>
+
+<style>
+    .pfp {
+        border-radius: 100%;
+    }
+</style>
